01d9ec3ef9725591bdfb2b316ce7c6173c176a5c
linqueta <lincolnrodrs@gmail.com>
2020-03-17 16:35:18 -0300

    add login

--- lib/mule/resources/session.rb
+++ lib/mule/resources/session.rb
@@ -9,12 +9,20 @@ module Mule
       MODEL = :session
 
       eezee_service :parse, lazy: true
-      eezee_request_options path: 'parse/sessions/:session_id'
+      eezee_request_options path: 'parse/:session_path'
 
       def self.find!(session_token)
         get(
-          params: { session_id: 'me' },
+          params: { session_path: 'sessions/me' },
           before: ->(req) { req.headers.merge!('X-Parse-Session-Token' => session_token) },
+          after: ->(_req, res) { handle!(res, MODEL) }
+        )
+      end
+
+      def self.login!(username, password)
+        get(
+          params: { session_path: 'login' },
+          payload: { username: username, password: password },
           after: ->(_req, res) { handle!(res, MODEL) }
         )
       end
